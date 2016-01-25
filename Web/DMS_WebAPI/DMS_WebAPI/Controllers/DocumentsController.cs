f806d4f13caef1cead2aac21c8b9d4e3c5f90fd6
stasupml <stasupml@gmail.com>
2016-01-25 13:06:18 +0200

    Add JsonResult.cs, DocumentsController.cs Set DefaultConnection by Owin Authorization

---
+++ Web/DMS_WebAPI/DMS_WebAPI/Controllers/DocumentsController.cs
@@ -0,0 +1,42 @@
+﻿using DMS_WebAPI.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DMS_WebAPI.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Documents")]
+    public class DocumentsController : ApiController
+    {
+        // GET: api/Documents
+        public IHttpActionResult Get()
+        {
+            return new JsonResult(new object[] { },this);
+        }
+
+        // GET: api/Documents/5
+        public IHttpActionResult Get(int id)
+        {
+            return new JsonResult(new { }, this);
+        }
+
+        // POST: api/Documents
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Documents/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Documents/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Web/DMS_WebAPI/DMS_WebAPI/Results/JsonResult.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace DMS_WebAPI.Results
+{
+    public class JsonResult : IHttpActionResult
+    {
+        object _data;
+        HttpRequestMessage _request;
+        bool _success;
+        string _msg;
+
+        public JsonResult(object data, ApiController controller)
+        {
+            _data = data;
+            _request = controller.Request;
+            _success = true;
+            _msg = string.Empty;
+        }
+        public JsonResult(object data, bool success, ApiController controller): this(data,controller)
+        {
+            _success = success;
+        }
+        public JsonResult(object data, string msg, ApiController controller) : this(data, controller)
+        {
+            _msg = msg;
+        }
+        public JsonResult(object data, bool success, string msg, ApiController controller):this(data,success,controller)
+        {
+            _msg = msg;
+        }
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            HttpResponseMessage response = _request.CreateResponse(HttpStatusCode.OK,new { success= _success, data= _data, msg= _msg });
+            return Task.FromResult(response);
+        }
+    }
+}
