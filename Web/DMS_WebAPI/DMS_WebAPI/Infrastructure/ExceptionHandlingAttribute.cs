bf158cf1751a740a559a3a2f2b13be5cdc8482c9
stasupml <stasupml@gmail.com>
2016-02-08 13:51:14 +0200

    Uncommented on the error handler

--- Web/DMS_WebAPI/DMS_WebAPI/Infrastructure/ExceptionHandlingAttribute.cs
+++ Web/DMS_WebAPI/DMS_WebAPI/Infrastructure/ExceptionHandlingAttribute.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BL.Model.Exception;
+using System;
+using System.Net;
+using System.Web;
 using System.Web.Http.Filters;
 
 namespace DMS_WebAPI.Infrastructure
@@ -8,88 +11,88 @@ namespace DMS_WebAPI.Infrastructure
     {
         public override void OnException(HttpActionExecutedContext context)
         {
-            //HttpContext.Current.Response.Clear();
-            //HttpContext.Current.Response.StatusCode = (int)HttpStatusCode.OK;
-            //HttpContext.Current.Response.ContentType = "application/json";
-            //if (context.Exception is DmsExceptions)
-            //{
-            //    if (context.Exception is UserUnauthorized)
-            //    {
-            //        HttpContext.Current.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-            //    }
-            //}
-            //else
-            //{
+            HttpContext.Current.Response.Clear();
+            HttpContext.Current.Response.StatusCode = (int)HttpStatusCode.OK;
+            HttpContext.Current.Response.ContentType = "application/json";
+            if (context.Exception is DmsExceptions)
+            {
+                if (context.Exception is UserUnauthorized)
+                {
+                    HttpContext.Current.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                }
+            }
+            else
+            {
 
-            //}
-            //HttpContext.Current.Response.Write(new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(new { success = false, msg = context.Exception.Message }));
-            //HttpContext.Current.Response.End();
-            //try
-            //{
-            //    var ex = context.Exception;
+            }
+            HttpContext.Current.Response.Write(new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(new { success = false, msg = context.Exception.Message }));
+            HttpContext.Current.Response.End();
+            try
+            {
+                var ex = context.Exception;
 
-            //    if (ex.InnerException != null) ex = ex.InnerException;
+                if (ex.InnerException != null) ex = ex.InnerException;
 
-            //    // stores the error message
-            //    string errorMessage = string.Empty;
-            //    errorMessage += "ERROR!!!\r\n";
-            //    try
-            //    {
-            //        HttpContext cnt = HttpContext.Current;
-            //        errorMessage += $"URL:{cnt.Request.Url.ToString()}\r\n";
-            //    }
-            //    catch
-            //    { }
+                // stores the error message
+                string errorMessage = string.Empty;
+                errorMessage += "ERROR!!!\r\n";
+                try
+                {
+                    HttpContext cnt = HttpContext.Current;
+                    errorMessage += $"URL:{cnt.Request.Url.ToString()}\r\n";
+                }
+                catch
+                { }
 
-            //    errorMessage += $"Message:{ex.Message}\r\n";
-            //    errorMessage += $"Source:{ex.Source}\r\n";
-            //    errorMessage += $"Method:{ex.TargetSite}\r\n";
-            //    errorMessage += $"StackTrace:{ex.StackTrace}\r\n";
-            //    errorMessage += $"Request:{context.Request}\r\n";
+                errorMessage += $"Message:{ex.Message}\r\n";
+                errorMessage += $"Source:{ex.Source}\r\n";
+                errorMessage += $"Method:{ex.TargetSite}\r\n";
+                errorMessage += $"StackTrace:{ex.StackTrace}\r\n";
+                errorMessage += $"Request:{context.Request}\r\n";
 
-            //    try
-            //    {
-            //        HttpContext.Current.Request.InputStream.Position = 0;
-            //        errorMessage += $"Request Body:{new System.IO.StreamReader(HttpContext.Current.Request.InputStream).ReadToEnd()}\r\n";
-            //    }
-            //    catch { }
+                try
+                {
+                    HttpContext.Current.Request.InputStream.Position = 0;
+                    errorMessage += $"Request Body:{new System.IO.StreamReader(HttpContext.Current.Request.InputStream).ReadToEnd()}\r\n";
+                }
+                catch { }
 
 
-            //    try
-            //    {
-            //        System.IO.StreamWriter sw;
-            //        string path = HttpContext.Current.Server.MapPath("~/SiteErrors.txt");
-            //        try
-            //        {
-            //            System.IO.FileInfo ff = new System.IO.FileInfo(path);
-            //            if (ff.Exists)
-            //            {
-            //            }
-            //        }
-            //        catch
-            //        {
+                try
+                {
+                    System.IO.StreamWriter sw;
+                    string path = HttpContext.Current.Server.MapPath("~/SiteErrors.txt");
+                    try
+                    {
+                        System.IO.FileInfo ff = new System.IO.FileInfo(path);
+                        if (ff.Exists)
+                        {
+                        }
+                    }
+                    catch
+                    {
 
-            //        }
+                    }
 
-            //        sw = System.IO.File.AppendText(path);
-            //        try
-            //        {
-            //            string line = DateTime.Now.ToString("o") + "\r\n" + errorMessage;
-            //            sw.WriteLine(line);
-            //        }
-            //        catch
-            //        {
-            //        }
-            //        finally
-            //        {
-            //            sw.Close();
-            //        }
-            //    }
-            //    catch
-            //    {
-            //    }
-            //}
-            //catch { }
+                    sw = System.IO.File.AppendText(path);
+                    try
+                    {
+                        string line = DateTime.Now.ToString("o") + "\r\n" + errorMessage;
+                        sw.WriteLine(line);
+                    }
+                    catch
+                    {
+                    }
+                    finally
+                    {
+                        sw.Close();
+                    }
+                }
+                catch
+                {
+                }
+            }
+            catch { }
         }
     }
 }
