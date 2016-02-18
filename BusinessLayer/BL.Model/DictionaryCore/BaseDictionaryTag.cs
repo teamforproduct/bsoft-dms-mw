bf69e80d15c8b946777935dbea96676eccd6d5b7
stasupml <stasupml@gmail.com>
2016-02-18 12:13:53 +0200

    add DictionaryTag

---
+++ BusinessLayer/BL.Model/DictionaryCore/BaseDictionaryTag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BL.Model.DictionaryCore
+{
+    public class BaseDictionaryTag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Nullable<int> PositionId { get; set; }
+        public string Color { get; set; }
+        public string PositionName { get; set; }
+    }
+}
