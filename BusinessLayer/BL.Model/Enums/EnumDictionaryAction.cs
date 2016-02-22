f8aad23074edebad63b2622c77c0db8f243db44c
alexandrmalinin <alexmalin@ukr.net>
2016-02-22 17:52:21 +0200

    Enum Actions

--- BusinessLayer/BL.Model/Enums/EnumDictionaryAction.cs
+++ BusinessLayer/BL.Model/Enums/EnumDictionaryAction.cs
@@ -2,7 +2,12 @@
 {
     public enum EnumDictionaryAction
     {
-        AddDocumentType,
-        ModifyDocumentType,
+        AddDocumentType = 201001, // Добавить тип документа
+        ModifyDocumentType = 201005, // Изменить тип документа
+        DeleteDocumentType = 201009, // Удалить тип документа
+
+        AddTag = 291001, // Добавить тэг
+        ModifyTag = 291005, // Изменить тэг
+        DeleteTag = 291009, // Удалить тэг
     }
 }
---
+++ BusinessLayer/BL.Model/Enums/EnumDocumentAditionActions.cs
@@ -0,0 +1,21 @@
+﻿namespace BL.Model.Enums
+{
+    public enum EnumDocumentAditionActions
+    {
+        AddDocumentRestrictedSendList = 102001, // Добавить ограничение рассылки
+        AddByStandartSendListDocumentRestrictedSendList = 102002, // Добавить ограничения рассылки по стандартному списку
+        DeleteDocumentRestrictedSendList = 102009, // Удалить ограничение рассылки
+        AddDocumentSendList = 103001, // Добавить пункт плана
+        AddByStandartSendListDocumentSendList = 103002, // Добавить план работы по стандартному списку
+        ModifyDocumentSendList = 103005, // Изменить документ
+        DeleteDocumentSendList = 103009, // Удалить пункт плана
+        AddDocumentSendListStage = 103011, // Добавить этап плана
+        DeleteDocumentSendListStage = 103019, // Удалить этап плана
+        AddDocumentFile = 104001, // Добавить файл
+        ModifyDocumentFile = 104005, // Изменить файл
+        DeleteDocumentFile = 104009, // Удалить файл
+        AddDocumentLinks = 105001, // Добавить связь между документами
+        DeleteDocumentLinks = 105009, // Удалить связь между документами
+        ModifyDocumentTags = 192005, // Изменить тэги по документу
+    }
+}
