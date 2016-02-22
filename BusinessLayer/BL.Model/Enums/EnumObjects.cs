6c766485120c3e4a877e20d7de1da380539c600d
alexandrmalinin <alexmalin@ukr.net>
2016-02-22 17:16:54 +0200

    ENUMs

--- BusinessLayer/BL.Model/Enums/EnumObjects.cs
+++ BusinessLayer/BL.Model/Enums/EnumObjects.cs
@@ -2,18 +2,18 @@
 {
     public enum EnumObjects
     {
-        Documents = 1, // Документы
-        DocumentAccesses = 11, // Документы - доступы
-        DocumentRestrictedSendLists = 12, // Документы - ограничения рассылки
-        DocumentSendLists = 13, // Документы - план работы
-        DocumentFiles = 14, // Документы - файлы
-        DocumentLinks = 15, // Документы - связи
-        DocumentEvents = 21, // Документы - события
-        DocumentWaits = 22, // Документы - ожидания
-        DocumentSubscriptions = 23, // Документы - подписи
-        DocumentSavedFilters = 91, // Документы - сохраненные фильтры
-        DocumentTags = 92, // Документы - тэги
-        DictionaryDocumentType = 101, // Типы документов
-        DictionaryTag = 201, // Теги
+        Documents = 100, // Документы
+        DocumentAccesses = 101, // Документы - доступы
+        DocumentRestrictedSendLists = 102, // Документы - ограничения рассылки
+        DocumentSendLists = 103, // Документы - план работы
+        DocumentFiles = 104, // Документы - файлы
+        DocumentLinks = 105, // Документы - связи
+        DocumentEvents = 111, // Документы - события
+        DocumentWaits = 112, // Документы - ожидания
+        DocumentSubscriptions = 113, // Документы - подписи
+        DocumentSavedFilters = 191, // Документы - сохраненные фильтры
+        DocumentTags = 192, // Документы - тэги
+        DictionaryDocumentType = 201, // Типы документов
+        DictionaryTag = 291, // Теги
     }
 }
