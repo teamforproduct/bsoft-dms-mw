cbade8efa362563fdb85d1f9ea899426a9e642fb
stasupml <stasupml@gmail.com>
2016-02-26 13:20:49 +0200

    Add field Task to SendLists

--- BusinessLayer/BL.Database/DBModel/Template/TemplateDocumentSendLists.cs
+++ BusinessLayer/BL.Database/DBModel/Template/TemplateDocumentSendLists.cs
@@ -15,6 +15,7 @@ namespace BL.Database.DBModel.Template
         public int DocumentId { get; set; }
         public int SendTypeId { get; set; }
         public Nullable<int> TargetPositionId { get; set; }
+        public string Task { get; set; }
         public string Description { get; set; }
         public int Stage { get; set; }
         public int? DueDay { get; set; }
