826039d70dfd108d7e4ee275e92db351e64b5421
sevtech04 <sevtech04@ukr.net>
2016-03-10 13:08:09 +0300

    DictionaryAgents - added plain models

--- BusinessLayer/BL.Model/DictionaryCore/FrontModel/FrontDictionaryAgent.cs
+++ BusinessLayer/BL.Model/DictionaryCore/FrontModel/FrontDictionaryAgent.cs
@@ -3,23 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BL.Model.Enums;
 
 namespace BL.Model.DictionaryCore.FrontModel
 {
+
+
+    /// <summary>
+    /// ОСНОВНОЙ. Справочник контрагентов
+    /// </summary>
     public class FrontDictionaryAgent
     {
+        /// <summary>
+        /// ИД
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// ФИО, Название, ...
+        /// </summary>
         public string Name { get; set; }
-        public bool IsIndividual { get; set; }
-        public bool IsEmployee { get; set; }
-        public bool IsBank { get; set; }
-        public bool IsCompany { get; set; }
-        public bool IsActive { get; set; }
-        public int ResidentTypeId { get; set; }
-        public string Description { get; set; }
 
-        public int LastChangeUserId { get; set; }
-        public DateTime LastChangeDate { get; set; }
+        /// <summary>
+        /// Кем в данный момент является контрагент
+        /// </summary>
+        public IEnumerable<EnumDictionaryAgentTypes> ActualTypes { get; set; }
+
+        //public bool IsIndividual { get; set; }
+        //public bool IsEmployee { get; set; }
+        //public bool IsBank { get; set; }
+        //public bool IsCompany { get; set; }
+        //public bool IsActive { get; set; }
+
+        /// <summary>
+        /// Резидентность
+        /// </summary>
+        public int ResidentTypeId { get; set; }
+        /// <summary>
+        /// Дополнительная информация
+        /// </summary>
+        public string Description { get; set; }
       
     }
 }
---
+++ BusinessLayer/BL.Model/DictionaryCore/InternalModel/InternalDictionaryAgent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL.Model.Common;
+
+namespace BL.Model.DictionaryCore.InternalModel
+{
+    class InternalDictionaryAgent : LastChangeInfo
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public bool IsCompany { get; set; }
+        public bool IsIndividual { get; set; }
+        public bool IsEmployee { get; set; }
+        public bool IsBank { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public int ResidentTypeId { get; set; }
+        public string Description { get; set; }
+    }
+}
