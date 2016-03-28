30194840027b765d5f146083a857003f07df9e57
sevtech04 <sevtech04@ukr.net>
2016-03-28 15:50:47 +0300

    fix

--- BusinessLayer/BL.Model/DictionaryCore/FilterModel/FilterCustomDictionary.cs
+++ BusinessLayer/BL.Model/DictionaryCore/FilterModel/FilterCustomDictionary.cs
@@ -4,6 +4,7 @@ namespace BL.Model.DictionaryCore.FilterModel
 {
     public class FilterCustomDictionary
     {
+        
         public List<int> CustomDictionaryTypeId { get; set; }
         public string Code { get; set; }
         public List<int> NotContainsId { get; set; }
