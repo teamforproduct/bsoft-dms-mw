46188fa7eff729df46f7a182a4b3c60a07ecab60
sevtech04 <sevtech04@ukr.net>
2016-03-29 10:52:50 +0300

    DictionaryStandartSendListContent

--- BusinessLayer/BL.Model/DictionaryCore/FilterModel/FilterDictionaryStandartSendListContent.cs
+++ BusinessLayer/BL.Model/DictionaryCore/FilterModel/FilterDictionaryStandartSendListContent.cs
@@ -1,10 +1,50 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace BL.Model.DictionaryCore.FilterModel
 {
+    /// <summary>
+    /// фильтр содержания типовой рассылки
+    /// </summary>
     public class FilterDictionaryStandartSendListContent
     {
-        public List<int> StandartSendListContentId { get; set; }
+
+        /// <summary>
+        /// список типовых рассылок
+        /// </summary>
+        public List<int> StandartSendListId { get; set; }
+        /// <summary>
+        /// игнорировать при поиске
+        /// </summary>
         public List<int> NotContainsId { get; set; }
+        /// <summary>
+        /// список типов рассылок
+        /// </summary>
+        public List<int> SendTypeId { get; set; }
+        /// <summary>
+        /// Должность получателя
+        /// </summary>
+        public int? TargetPositionId { get; set; }
+        /// <summary>
+        /// ПОльзователь-получатель
+        /// </summary>
+        public int? TargetAgentId { get; set; }
+        /// <summary>
+        /// Задача
+        /// </summary>
+        public string Task { get; set; }
+        /// <summary>
+        /// Название типа рассылки
+        /// </summary>
+        public string SendTypeName { get; set; }
+        /// <summary>
+        /// Название должности получателя
+        /// </summary>
+        public string TargetPositionName { get; set; }
+        /// <summary>
+        /// ФИО получателя
+        /// </summary>
+        public string TargetAgentName { get; set; }
+
     }
 }
