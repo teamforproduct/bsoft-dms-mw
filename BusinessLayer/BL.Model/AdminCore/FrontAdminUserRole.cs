2c32021bf7178ad5c79c558d274611ca0b83a57b
sergey.zakharov <sergey.zakharov@artcom-venture.de>
2016-04-24 17:14:14 +0200

    fix autoplan selection, add event and waits availability with Task add new events count to the user role list

---
+++ BusinessLayer/BL.Model/AdminCore/FrontAdminUserRole.cs
@@ -0,0 +1,13 @@
+﻿namespace BL.Model.AdminCore
+{
+    /// <summary>
+    /// описывает доступные пользователю роли
+    /// </summary>
+    public class FrontAdminUserRole
+    {
+        public int RolePositionId { get; set; }
+        public string RolePositionName { get; set; }
+        public string RolePositionExecutorAgentName { get; set; }
+        public int NewEventsCount { get; set; }
+    }
+}
