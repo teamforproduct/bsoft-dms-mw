2a82ea578cbfaa5049ac7b8c643d83182bfb4b6f
stasupml <stasupml@gmail.com>
2016-05-04 17:19:40 +0300

    Add page for Activation Program

---
+++ Web/DMS_WebAPI/DMS_WebAPI/Controllers/System/LicenseController.cs
@@ -0,0 +1,29 @@
+﻿using DMS_WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DMS_WebAPI.Controllers
+{
+    public class LicenseController : Controller
+    {
+        public ActionResult ActivationProgram()
+        {
+            return View(new ActivationProgramViewModel { ProgramCode = "spfJA7G6UW/Y+53YjhUPEkGi4YA3GZX1Ci4aIkzh24lj4nJGW1GiuX4V1QGpxjZuMiy" });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ActivationProgram(ActivationProgramViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Web/DMS_WebAPI/DMS_WebAPI/Models/LicenseViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DMS_WebAPI.Models
+{
+    public class ActivationProgramViewModel
+    {
+        [Display(Name = "Ваш код программы")]
+        public string ProgramCode { get; set; }
+
+        [Required(ErrorMessage = "Введите код активации!")]
+        //[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "Код активации")]
+        public string ActivationCode { get; set; }
+    }
+}
