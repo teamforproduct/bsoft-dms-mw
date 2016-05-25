b416e0b619f7e234a3c6dd44ac01e8595430ac44
stasupml <stasupml@gmail.com>
2016-05-25 13:43:22 +0300

    Работа с файлами

--- Web/DMS_WebAPI/DMS_WebAPI/Utilities/WebAPIDbProcess.cs
+++ Web/DMS_WebAPI/DMS_WebAPI/Utilities/WebAPIDbProcess.cs
@@ -44,7 +44,8 @@ namespace DMS_WebAPI.Utilities
 
                 if (filter.ServerTypes?.Count > 0)
                 {
-                    qry = qry.Where(x => filter.ServerTypes.Cast<string>().Contains(x.ServerType));
+                    var serverTypes = filter.ServerTypes.Select(x => x.ToString()).ToList();
+                    qry = qry.Where(x => serverTypes.Contains(x.ServerType));
                 }
             }
 
