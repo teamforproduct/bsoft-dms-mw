b36626327e3dc399f00c09892830d1447b16c006
stasupml <stasupml@gmail.com>
2016-06-01 12:42:18 +0300

    Logger

---
+++ BusinessLayer/BL.CrossCutting/Helpers/Logger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.CrossCutting.Helpers
+{
+    public class Logger
+    {
+        public static void SaveToFile(string method, TimeSpan elapsed)
+        {
+            SaveToFile(method, String.Format("{0:0.00000000}", elapsed.TotalSeconds));
+        }
+        private static void SaveToFile(string method, string time)
+        {
+            try
+            {
+                System.IO.StreamWriter sw = System.IO.File.AppendText(System.Web.HttpContext.Current.Server.MapPath("~/SiteLog.txt"));
+                try
+                {
+                    string line = $"{method};{System.DateTime.Now.ToString("o")};{time}";
+                    sw.WriteLine(line);
+                }
+                catch
+                {
+                }
+                finally
+                {
+                    sw.Close();
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
