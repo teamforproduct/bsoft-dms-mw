0105bb92e9e7fb76aa7613c647147fc33ec5bbcc
sergey <sergozaharov@gmail.com>
2016-06-23 19:50:02 +0200

    modify full text reindex

--- BusinessLayer/BusinessLayer/SystemServices/FullTextSearch/FullTextSearchService.cs
+++ BusinessLayer/BusinessLayer/SystemServices/FullTextSearch/FullTextSearchService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using BL.CrossCutting.Interfaces;
 using BL.Database.SystemDb;
 using BL.Logic.Common;
@@ -43,7 +44,7 @@ namespace BL.Logic.SystemServices.FullTextSearch
             worker.StartUpdate();
             try
             {
-                int offset =0;
+                
                 var currCashId = _systemDb.GetCurrentMaxCasheId(ctx);
                 var objToProcess = new EnumObjects[]
                 {
@@ -52,37 +53,46 @@ namespace BL.Logic.SystemServices.FullTextSearch
                 };
                 //delete all current document before reindexing
                 worker.DeleteAllDocuments();
-
+                var tskList = new List<Task>();
                 //going through the documents objects and add it. Select max 1000 row for one processing
                 foreach (var dataType in objToProcess)
                 {
-                    do
+                    tskList.Add(Task.Factory.StartNew(() =>
                     {
-                        var data = _systemDb.FullTextIndexDocumentsReindexDbPrepare(ctx, dataType, MAX_ROW_PROCESS,offset);
-                        foreach (var itm in data)
+                        int offset = 0;
+                        do
                         {
-                            worker.AddNewItem(itm);
-                        }
 
-                        if (data.Count() == MAX_ROW_PROCESS)
-                        {
-                            offset += MAX_ROW_PROCESS;
-                        }
-                        else
-                        {
-                            offset = 0;
-                        }
+                            var data = _systemDb.FullTextIndexDocumentsReindexDbPrepare(ctx, dataType, MAX_ROW_PROCESS,offset);
+                            foreach (var itm in data)
+                            {
+                                worker.AddNewItem(itm);
+                            }
 
-                    } while (offset != 0);
+                            if (data.Count() == MAX_ROW_PROCESS)
+                            {
+                                offset += MAX_ROW_PROCESS;
+                            }
+                            else
+                            {
+                                offset = 0;
+                            }
 
+                        } while (offset != 0);
+                    }));
                 }
 
-                // add to index dictionaries and templates 
-                var additionaData = _systemDb.FullTextIndexNonDocumentsReindexDbPrepare(ctx);
-                foreach (var itm in additionaData)
+                tskList.Add(Task.Factory.StartNew(() =>
                 {
-                    worker.AddNewItem(itm);
-                }
+                    // add to index dictionaries and templates 
+                    var additionaData = _systemDb.FullTextIndexNonDocumentsReindexDbPrepare(ctx);
+                    foreach (var itm in additionaData)
+                    {
+                        worker.AddNewItem(itm);
+                    }
+                }));
+
+                Task.WaitAll(tskList.ToArray());
 
                 //delete cash in case we just processed all that documents
                 _systemDb.FullTextIndexDeleteCash(ctx, currCashId);
