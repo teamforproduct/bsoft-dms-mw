66718751123cb6d267069630c44975da99c644ae
SergeyPauls <sergey_pauls@mail.ru>
2016-09-29 09:26:31 +0300

    Get AssemblyVersion

---
+++ Web/DMS_WebAPI/DMS_WebAPI/Controllers/System/VersionController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using BL.CrossCutting.DependencyInjection;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using DMS_WebAPI.Models;
+using DMS_WebAPI.Providers;
+using DMS_WebAPI.Results;
+using BL.Logic.DependencyInjection;
+using DMS_WebAPI.Utilities;
+using System.Reflection;
+
+namespace DMS_WebAPI.Controllers
+{
+    public class VersionController : ApiController
+    {
+        /// <summary>
+        /// Возвращает версию сборки
+        /// </summary>
+        /// <param name="filter">Filter parms</param>
+        /// <returns>FrontAdminPositions</returns>
+        public IHttpActionResult Get()
+        {
+            var tmpItems = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            return new JsonResult(tmpItems, this);
+        }
+
+    }
+}
