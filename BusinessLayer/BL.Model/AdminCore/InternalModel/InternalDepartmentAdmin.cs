8b63a4ce5c7a3564f8d2fc82a8ab370ea66f765b
SergeyPauls <sergey_pauls@mail.ru>
2016-10-05 20:28:34 +0300

    Add files for DepartmentAdmins

---
+++ BusinessLayer/BL.Model/AdminCore/InternalModel/InternalDepartmentAdmin.cs
@@ -0,0 +1,30 @@
+﻿using BL.Model.Common;
+using BL.Model.AdminCore.IncomingModel;
+
+namespace BL.Model.AdminCore.InternalModel
+{
+    public class InternalDepartmentAdmin : LastChangeInfo
+    {
+        public InternalDepartmentAdmin()
+        { }
+
+        public InternalDepartmentAdmin(ModifyAdminDepartmentAdmin model)
+        {
+            DepartmentId = model.DepartmentId;
+            EmployeeId = model.EmployeeId;
+        }
+
+        /// <summary>
+        /// Отдел
+        /// </summary>
+        public int DepartmentId { get; set; }
+
+        /// <summary>
+        /// Сотрудник
+        /// </summary>
+        public int EmployeeId { get; set; }
+
+        public string EmployeeName { get; set; }
+
+    }
+}
---
+++ BusinessLayer/BusinessLayer/DictionaryCore/AgentAccount/BaseDictionaryAgentAccountCommand.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using BL.Logic.Common;
+using BL.Model.DictionaryCore.IncomingModel;
+using BL.Model.DictionaryCore.InternalModel;
+using BL.Model.Exception;
+using BL.Model.DictionaryCore.FilterModel;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BL.Logic.DictionaryCore
+{
+    public class BaseDictionaryAgentAccountCommand : BaseDictionaryCommand
+    {
+        protected ModifyDictionaryAgentAccount Model
+        {
+            get
+            {
+                if (!(_param is ModifyDictionaryAgentAccount))
+                {
+                    throw new WrongParameterTypeError();
+                }
+                return (ModifyDictionaryAgentAccount)_param;
+            }
+        }
+
+        public override bool CanBeDisplayed(int positionId) => true;
+
+        public override bool CanExecute()
+        {
+            _adminService.VerifyAccess(_context, CommandType, false);
+
+            Model.AccountNumber?.Trim();
+
+            var spr = _dictDb.GetAgentAccounts(_context, new FilterDictionaryAgentAccount
+            {
+                AgentId = Model.AgentId,
+                AccountNumber = Model.AccountNumber,
+                NotContainsIDs = new List<int> { Model.Id }
+            });
+
+            if (spr.Count() != 0)
+            {
+                throw new DictionaryAgentAccountNumberNotUnique(Model.AccountNumber);
+            }
+
+            return true;
+        }
+
+        public override object Execute()
+        {
+            if (Model.IsMain) SetMainAgentAccount();
+            return null;
+        }
+
+        private void SetMainAgentAccount()
+        {
+            var accounts = _dictDb.GetInternalAgentAccounts(_context, new FilterDictionaryAgentAccount() { AgentId = Model.AgentId });
+
+            foreach (InternalDictionaryAgentAccount account in accounts)
+            {
+                if (account.Id != Model.Id)
+                {
+                    account.IsMain = false;
+                    _dictDb.UpdateAgentAccount(_context, account);
+                }
+            }
+        }
+    }
+}
