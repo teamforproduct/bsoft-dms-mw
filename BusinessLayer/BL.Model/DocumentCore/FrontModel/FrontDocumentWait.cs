07bb89dd9a6bcd4b2c2c837a54900b0b831b31a9
alexandrmalinin <alexmalin@ukr.net>
2016-10-05 16:08:18 +0300

    wait GroupsCounter

--- BusinessLayer/BL.Model/DocumentCore/FrontModel/FrontDocumentWait.cs
+++ BusinessLayer/BL.Model/DocumentCore/FrontModel/FrontDocumentWait.cs
@@ -19,6 +19,11 @@ namespace BL.Model.DocumentCore.FrontModel
         public FrontDocumentEvent OnEvent { get; set; }
         public FrontDocumentEvent OffEvent { get; set; }
         public bool IsClosed { get; set; }
+        public bool IsOverDue { get; set; }
+        public string SourcePositionExecutorAgentName { get; set; }
+        public string TargetPositionExecutorAgentName { get; set; }
+        public int? RecordCount { get; set; }
+
 
 
     }
