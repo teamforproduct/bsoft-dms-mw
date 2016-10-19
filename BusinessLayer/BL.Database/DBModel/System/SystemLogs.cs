e149bfd319673c182649b9dd77a806071ddf2264
alexandrmalinin <alexmalin@ukr.net>
2016-10-19 22:01:36 +0300

    Migrations/201610191859186_changeLogs.cs

--- BusinessLayer/BL.Database/DBModel/System/SystemLogs.cs
+++ BusinessLayer/BL.Database/DBModel/System/SystemLogs.cs
@@ -17,7 +17,15 @@ namespace BL.Database.DBModel.System
         [MaxLength(2000)]
         public string LogException { get; set; }
         public Nullable<int> ExecutorAgentId { get; set; }
+        public Nullable<int> ObjectId { get; set; }
+        public Nullable<int> ActionId { get; set; }
+        public Nullable<int> RecordId { get; set; }
+
         [Index("IX_LogDate", 1)]
         public DateTime LogDate { get; set; }
+        [ForeignKey("ObjectId")]
+        public virtual SystemObjects Object { get; set; }
+        [ForeignKey("ActionId")]
+        public virtual SystemActions Action { get; set; }
     }
 }
