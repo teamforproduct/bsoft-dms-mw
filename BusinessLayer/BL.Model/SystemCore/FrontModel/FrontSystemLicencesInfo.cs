fad98d2d8064c825995152932bbe8132723ba8b2
alexandrmalinin <alexmalin@ukr.net>
2016-11-15 12:41:37 +0200

    VerifyLicences

---
+++ BusinessLayer/BL.Model/SystemCore/FrontModel/FrontSystemLicencesInfo.cs
@@ -0,0 +1,25 @@
+﻿using BL.Model.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BL.Model.SystemCore.FrontModel
+{
+    /// <summary>
+    /// описание результатов проверки лицензии
+    /// </summary>
+    public class FrontSystemLicencesInfo
+    {
+        /// <summary>
+        /// код важности сообщения
+        /// </summary>
+        public EnumMessageLevelTypes MessageLevelTypes { get; set; }
+        /// <summary>
+        /// название уровня важности сообщения
+        /// </summary>
+        public string MessageLevelTypesName { get; set; }
+        /// <summary>
+        /// сообщение
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
