502914da36bf0a4af1271743ff462ce3bd688d5f
PaulsSergey <sergey_pauls@mail.ru>
2016-12-19 23:44:25 +0300

    V3 User API

---
+++ BusinessLayer/BL.Model/Users/FrontPermission.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Model.Users
+{
+    public class FrontPermission
+    {
+        /// <summary>
+        /// Модуль
+        /// </summary>
+        public string module { get; set; }
+
+        /// <summary>
+        /// Фича
+        /// </summary>
+        public string feature { get; set; }
+
+        /// <summary>
+        /// Доступ по CRUD
+        /// </summary>
+        public string accessType { get; set; }
+
+    }
+}
