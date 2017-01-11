8426c46517b90377a21a09e2d9c5827edc18d395
PaulsSergey <sergey_pauls@mail.ru>
2017-01-11 14:44:39 +0300

    V3 AiditLog

---
+++ Web/DMS_WebAPI/DMS_WebAPI/ControllersV3/Auth/AuthInfoController.cs
@@ -0,0 +1,98 @@
+﻿using BL.Logic.DictionaryCore.Interfaces;
+using BL.Model.DictionaryCore.FilterModel;
+using BL.Model.DictionaryCore.IncomingModel;
+using BL.Model.DictionaryCore.FrontModel;
+using BL.Model.Enums;
+using DMS_WebAPI.Results;
+using DMS_WebAPI.Utilities;
+using System.Web.Http;
+using BL.Model.SystemCore;
+using BL.CrossCutting.DependencyInjection;
+using System.Web.Http.Description;
+using System.Collections.Generic;
+
+using BL.Model.Common;
+using System.Web;
+using BL.Logic.SystemServices.TempStorage;
+using BL.Model.DictionaryCore.FrontMainModel;
+using System.Diagnostics;
+using BL.Model.FullTextSearch;
+
+namespace DMS_WebAPI.ControllersV3.Auth
+{
+    /// <summary>
+    /// Банки
+    /// </summary>
+    [Authorize]
+    [RoutePrefix(ApiPrefix.V3 + ApiPrefix.Auth)]
+    public class AuthInfoController : ApiController
+    {
+        Stopwatch stopWatch = new Stopwatch();
+
+        /// <summary>
+        /// Возвращает реквизиты банка
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("Info/{Id:int}")]
+        [ResponseType(typeof(FrontAgentBank))]
+        public IHttpActionResult Get(int Id)
+        {
+            if (!stopWatch.IsRunning) stopWatch.Restart();
+            var ctx = DmsResolver.Current.Get<UserContexts>().Get();
+            var tmpService = DmsResolver.Current.Get<IDictionaryService>();
+            var tmpItem = tmpService.GetAgentBank(ctx, Id);
+            var res = new JsonResult(tmpItem, this);
+            res.SpentTime = stopWatch;
+            return res;
+        }
+
+        /// <summary>
+        /// Добавляет банк
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("Info")]
+        public IHttpActionResult Post([FromBody]AddAgentBank model)
+        {
+            if (!stopWatch.IsRunning) stopWatch.Restart();
+            var tmpItem = Action.Execute(EnumDictionaryActions.AddAgentBank, model);
+            return Get(tmpItem);
+        }
+
+        /// <summary>
+        /// Корректирует реквизиты банка
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("Info")]
+        public IHttpActionResult Put([FromBody]ModifyAgentBank model)
+        {
+            if (!stopWatch.IsRunning) stopWatch.Restart();
+            Action.Execute(EnumDictionaryActions.ModifyAgentBank, model);
+            return Get(model.Id);
+        }
+
+        /// <summary>
+        /// Удаляет банк
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("Info/{Id:int}")]
+        public IHttpActionResult Delete([FromUri] int Id)
+        {
+            if (!stopWatch.IsRunning) stopWatch.Restart();
+            Action.Execute(EnumDictionaryActions.DeleteAgentBank, Id);
+            var tmpItem = new FrontDeleteModel(Id);
+            var res = new JsonResult(tmpItem, this);
+            res.SpentTime = stopWatch;
+            return res;
+        }
+
+    }
+}
