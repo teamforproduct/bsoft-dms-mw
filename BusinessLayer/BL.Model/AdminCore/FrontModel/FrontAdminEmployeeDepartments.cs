c366f0b24b8f54c8a311c47c4cb34dca9dea4377
PaulsSergey <sergey_pauls@mail.ru>
2017-01-15 23:19:21 +0300

    V3 AdminDepartments

---
+++ BusinessLayer/BL.Model/AdminCore/FrontModel/FrontAdminEmployeeDepartments.cs
@@ -0,0 +1,32 @@
+﻿using BL.Model.AdminCore.IncomingModel;
+using BL.Model.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace BL.Model.AdminCore.FrontModel
+{
+    public class FrontAdminEmployeeDepartments 
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Сотрудник
+        /// </summary>
+        public int EmployeeId { get; set; }
+
+        /// <summary>
+        /// Сотрудник
+        /// </summary>
+        public string EmployeeName { get; set; }
+
+        /// <summary>
+        /// Отдел
+        /// </summary>
+        public int DepartmentId { get; set; }
+
+
+    }
+}
