f11a413e70223ab7f7e5767f4bbb86a1cc762709
Sergey <sergey.zakharov@artcom-venture.de>
2017-01-25 15:12:30 +0100

    add authorize attribute

--- Web/DMS_WebAPI/DMS_WebAPI/ControllersV3/Documents/DocumentEventController.cs
+++ Web/DMS_WebAPI/DMS_WebAPI/ControllersV3/Documents/DocumentEventController.cs
@@ -1,17 +1,11 @@
 ﻿using BL.CrossCutting.DependencyInjection;
-using BL.Logic.DictionaryCore.Interfaces;
 using BL.Logic.DocumentCore.Interfaces;
-using BL.Model.Common;
-using BL.Model.DictionaryCore.FilterModel;
-using BL.Model.DictionaryCore.FrontModel;
-using BL.Model.DictionaryCore.IncomingModel;
 using BL.Model.DictionaryCore.InternalModel;
 using BL.Model.DocumentCore.Actions;
 using BL.Model.DocumentCore.Filters;
 using BL.Model.DocumentCore.FrontModel;
 using BL.Model.DocumentCore.IncomingModel;
 using BL.Model.Enums;
-using BL.Model.FullTextSearch;
 using BL.Model.SystemCore;
 using DMS_WebAPI.Results;
 using DMS_WebAPI.Utilities;
@@ -27,6 +21,7 @@ namespace DMS_WebAPI.ControllersV3.Documents
     /// Документы. События.
     /// </summary>
     [Authorize]
+    [DimanicAuthorizeAttribute]
     [RoutePrefix(ApiPrefix.V3 + Modules.Documents)]
     public class DocumentEventController : ApiController
     {
--- Web/DMS_WebAPI/DMS_WebAPI/Utilities/DimanicAuthorizeAttribute.cs
+++ Web/DMS_WebAPI/DMS_WebAPI/Utilities/DimanicAuthorizeAttribute.cs
@@ -6,14 +6,38 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
+using BL.CrossCutting.DependencyInjection;
+using BL.Model.Enums;
 
 
 namespace DMS_WebAPI.Utilities
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class DimanicAuthorizeAttribute : AuthorizeAttribute, IAuthorizationFilter
     {
-        public bool AllowMultiple {
-            get { return false; }
+        public override bool AllowMultiple { get; } = false;
+        private EnumAccessTypes? _operationRight;
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="operationRight"></param>
+        public DimanicAuthorizeAttribute(string operationRight = null)
+        {
+            _operationRight = null;
+
+            if (!string.IsNullOrEmpty(operationRight))
+            {
+                switch (operationRight)
+                {
+                    case "C": _operationRight = EnumAccessTypes.C; break;
+                    case "U": _operationRight = EnumAccessTypes.U; break;
+                    case "D": _operationRight = EnumAccessTypes.D; break;
+                    case "R": _operationRight = EnumAccessTypes.R; break;
+                }
+            }
         }
 
         public Task<HttpResponseMessage> ExecuteAuthorizationFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken,
@@ -22,22 +46,56 @@ namespace DMS_WebAPI.Utilities
             var principal = actionContext.RequestContext.Principal;
             if (principal == null)
             {
-                var controller = actionContext.ControllerContext.ControllerDescriptor.ControllerName;
-                var method = actionContext.ActionDescriptor.ActionName;
+                //var controller = actionContext.ControllerContext.ControllerDescriptor.ControllerName;
+                //var method = actionContext.ActionDescriptor.ActionName;
                 return Task.FromResult<HttpResponseMessage>(actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized));
+            }
+
+            var uri = actionContext.Request.RequestUri.AbsolutePath;
+
+            if (!uri.StartsWith("/api/v3/")) Task.FromResult<HttpResponseMessage>(actionContext.Request.CreateResponse(HttpStatusCode.Forbidden));
+
+            var param =uri.Replace("/api/v3/", "").Split('/');
+
+            if (param.Length <2) Task.FromResult<HttpResponseMessage>(actionContext.Request.CreateResponse(HttpStatusCode.Forbidden));
+
+            string module = param[0];
+            string feature;
+            int valueParsed;
+            if (Int32.TryParse(param[1], out valueParsed) && param.Length >= 3)
+            {
+                feature = param[2];
             }
             else
             {
+                feature = param[1];
+            }
+
+            if (_operationRight == null)
+            {
+                switch (actionContext.Request.Method.Method)
+                {
+                    case "POST": _operationRight = EnumAccessTypes.C; break;
+                    case "PUT": _operationRight = EnumAccessTypes.U; break;
+                    case "GET": _operationRight = EnumAccessTypes.R; break;
+                    case "DELETE": _operationRight = EnumAccessTypes.D; break;
+                }
+            }
+
+            if (CheckRight(module, feature, _operationRight.Value))
+            {
                 return continuation();
             }
+
+            return Task.FromResult<HttpResponseMessage>(actionContext.Request.CreateResponse(HttpStatusCode.NotAcceptable));
         }
 
-        public void OnAuthorization(System.Web.Http.Controllers.HttpActionContext filterContext)
+
+        private bool CheckRight(string module, string feature, EnumAccessTypes right)
         {
-            string action = filterContext.ActionDescriptor.ActionName;
-            string controller = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
-            //string user = filterContext.HttpContext.User.Identity.Name;
-            var i = 1;
+            var ctx = DmsResolver.Current.Get<UserContexts>().Get();
+            return true;
         }
+
     }
 }
