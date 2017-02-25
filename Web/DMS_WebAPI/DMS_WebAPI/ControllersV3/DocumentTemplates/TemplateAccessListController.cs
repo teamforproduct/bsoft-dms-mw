991c81bcd278c204d7fe166231190c3e16a7ca44
PaulsSergey <sergey_pauls@mail.ru>
2017-02-25 03:16:32 +0300

    fix Namespace

--- Web/DMS_WebAPI/DMS_WebAPI/ControllersV3/DocumentTemplates/TemplateAccessListController.cs
+++ Web/DMS_WebAPI/DMS_WebAPI/ControllersV3/DocumentTemplates/TemplateAccessListController.cs
@@ -13,7 +13,7 @@ using System.Diagnostics;
 using System.Web.Http;
 using System.Web.Http.Description;
 
-namespace DMS_WebAPI.ControllersV3.DocumentTemlates
+namespace DMS_WebAPI.ControllersV3.DocumentTemplates
 {
     /// <summary>
     /// Шаблоны документов. Ограничительные списки рассылки.
