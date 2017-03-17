6328d1e5d829ad73432095f5c77cbaca0ff62e12
alexandrmalinin <alexmalin@ukr.net>
2017-03-17 12:17:06 +0200

    fix

--- BusinessLayer/BusinessLayer/DocumentCore/Commands/AddDocumentCommand.cs
+++ BusinessLayer/BusinessLayer/DocumentCore/Commands/AddDocumentCommand.cs
@@ -82,7 +82,7 @@ namespace BL.Logic.DocumentCore.Commands
             CommonDocumentUtilities.SetSendListAtrributesForNewDocument(_context, _document.SendLists, true);
             CommonDocumentUtilities.SetLastChange(_context, _document.RestrictedSendLists);
 
-            Document.Events = CommonDocumentUtilities.GetNewDocumentEvents(_context, null, EnumEventTypes.AddNewDocument);
+            Document.Events = CommonDocumentUtilities.GetNewDocumentEvents(_context, (int)EnumEntytiTypes.Document, null, EnumEventTypes.AddNewDocument);
             Document.Accesses = CommonDocumentUtilities.GetNewDocumentAccesses(_context, (int)EnumEntytiTypes.Document);
 
             // prepare file list in Document. It will save it with document in DB
