6c62c88e74a849959e7882063bca231e5c98d480
PaulsSergey <sergey_pauls@mail.ru>
2017-03-20 07:57:14 +0300

    Add User/SendList API v1

---
+++ Web/DMS_WebAPI/DMS_WebAPI/ControllersV3/User/UserSendListContentsController.cs
@@ -0,0 +1,115 @@
+﻿using BL.Logic.DictionaryCore.Interfaces;
+using BL.Model.DictionaryCore.IncomingModel;
+using BL.Model.DictionaryCore.FrontModel;
+using DMS_WebAPI.Results;
+using DMS_WebAPI.Utilities;
+using System.Web.Http;
+using BL.Model.Enums;
+using BL.Model.DictionaryCore.FilterModel;
+using BL.CrossCutting.DependencyInjection;
+using System.Web.Http.Description;
+using System.Collections.Generic;
+using BL.Model.Common;
+using System.Diagnostics;
+using BL.Model.SystemCore;
+
+namespace DMS_WebAPI.ControllersV3.User
+{
+    /// <summary>
+    /// Списки рассылки. Пункты
+    /// </summary>
+    [Authorize]
+    //![DimanicAuthorize]
+    [RoutePrefix(ApiPrefix.V3 + Modules.User)]
+    public class UserSendListContentsController : ApiController
+    {
+        Stopwatch stopWatch = new Stopwatch();
+
+        /// <summary>
+        /// Возвращает список рассылки
+        /// </summary>
+        /// <param name="Id">ИД сотрудника</param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route(Features.SendLists +  "/{Id:int}/" + Features.Contents)]
+        [ResponseType(typeof(List<FrontDictionaryStandartSendListContent>))]
+        public IHttpActionResult Get(int Id, [FromUri] FilterDictionaryStandartSendListContent filter)
+        {
+            if (!stopWatch.IsRunning) stopWatch.Restart();
+            if (filter == null) filter = new FilterDictionaryStandartSendListContent();
+            filter.StandartSendListId = new List<int> { Id };
+
+            var ctx = DmsResolver.Current.Get<UserContexts>().Get();
+            var tmpService = DmsResolver.Current.Get<IDictionaryService>();
+            var tmpItems = tmpService.GetDictionaryStandartSendListContents(ctx, filter);
+            var res = new JsonResult(tmpItems, this);
+            res.SpentTime = stopWatch;
+            return res;
+        }
+
+        /// <summary>
+        /// Возвращает пункт списка рассылки по Id
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route(Features.SendLists + "/" + Features.Contents + " /{Id:int}")]
+        [ResponseType(typeof(FrontDictionaryStandartSendListContent))]
+        public IHttpActionResult Get(int Id)
+        {
+            if (!stopWatch.IsRunning) stopWatch.Restart();
+            var ctx = DmsResolver.Current.Get<UserContexts>().Get();
+            var tmpService = DmsResolver.Current.Get<IDictionaryService>();
+            var tmpItem = tmpService.GetDictionaryStandartSendListContent(ctx, Id);
+            var res = new JsonResult(tmpItem, this);
+            res.SpentTime = stopWatch;
+            return res;
+        }
+
+        /// <summary>
+        /// Создает новый пункт списка рассылки
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route(Features.SendLists + "/" + Features.Contents)]
+        public IHttpActionResult Post([FromBody]AddStandartSendListContent model)
+        {
+            if (!stopWatch.IsRunning) stopWatch.Restart();
+            var tmpItem = Action.Execute(EnumDictionaryActions.AddStandartSendListContent, model);
+            return Get(tmpItem);
+        }
+
+        /// <summary>
+        /// Корректирует пункт списка рассылки
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route(Features.SendLists + "/" + Features.Contents)]
+        public IHttpActionResult Put([FromBody]ModifyStandartSendListContent model)
+        {
+            if (!stopWatch.IsRunning) stopWatch.Restart();
+            Action.Execute(EnumDictionaryActions.ModifyStandartSendListContent, model);
+            return Get(model.Id);
+        }
+
+        /// <summary>
+        /// Удаляет пункт списка рассылки
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route(Features.SendLists + "/" + Features.Contents + "/{Id:int}")]
+        public IHttpActionResult Delete([FromUri] int Id)
+        {
+            if (!stopWatch.IsRunning) stopWatch.Restart();
+            Action.Execute(EnumDictionaryActions.DeleteStandartSendListContent, Id);
+            var tmpItem = new FrontDeleteModel(Id);
+            var res = new JsonResult(tmpItem, this);
+            res.SpentTime = stopWatch;
+            return res;
+        }
+    }
+}
