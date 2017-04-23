7fd8ae963984ee14652e78665acbd0efe16d15f3
sergey <sergozaharov@gmail.com>
2017-04-23 16:29:22 +0200

    попытка добавления люцены

--- BusinessLayer/BusinessLayer/SystemServices/FullTextSearch/CaseInsensitiveWhitespaceAnalyzer.cs
+++ BusinessLayer/BusinessLayer/SystemServices/FullTextSearch/CaseInsensitiveWhitespaceAnalyzer.cs
@@ -8,17 +8,17 @@ using System.Threading.Tasks;
 
 namespace BL.Logic.SystemServices.FullTextSearch
 {
-    public class CaseInsensitiveWhitespaceAnalyzer : Analyzer
-    {
-        /// <summary>
-        /// </summary>
-        public override TokenStream TokenStream(string fieldName, TextReader reader)
-        {
-            TokenStream t = null;
-            t = new WhitespaceTokenizer(reader);
-            t = new LowerCaseFilter(t);
+    //public class CaseInsensitiveWhitespaceAnalyzer : Analyzer
+    //{
+    //    /// <summary>
+    //    /// </summary>
+    //    public override TokenStream TokenStream(string fieldName, TextReader reader)
+    //    {
+    //        TokenStream t = null;
+    //        t = new WhitespaceTokenizer(reader);
+    //        t = new LowerCaseFilter(t);
 
-            return t;
-        }
-    }
+    //        return t;
+    //    }
+    //}
 }
