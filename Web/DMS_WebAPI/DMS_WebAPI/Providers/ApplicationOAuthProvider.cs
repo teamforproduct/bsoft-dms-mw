3eb5dc6cf0cc5c2df603506d3b0f206fa414d12a
PaulsSergey <sergey_pauls@mail.ru>
2017-04-26 00:15:50 +0300

    fix

--- Web/DMS_WebAPI/DMS_WebAPI/Providers/ApplicationOAuthProvider.cs
+++ Web/DMS_WebAPI/DMS_WebAPI/Providers/ApplicationOAuthProvider.cs
@@ -76,7 +76,8 @@ namespace DMS_WebAPI.Providers
             if (string.IsNullOrEmpty(clientCode?.Trim())) throw new ClientCodeRequired();
 
             // отпечаток - обязательный параметр (решили сделать обязательным, хотя дальше по логике он может не понадобиться)
-            if (string.IsNullOrEmpty(fingerprint?.Trim())) throw new FingerprintRequired();
+            // не можем передавать из соапа
+            //if (string.IsNullOrEmpty(fingerprint?.Trim())) throw new FingerprintRequired();
 
             // Если передали несуществующие код клиента. дальше не пускаю
             var webService = DmsResolver.Current.Get<WebAPIService>();
